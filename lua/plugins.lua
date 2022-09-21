da379d2df6c8fd5bc94066b9af2b8b7fcc9a4e39
shuo chang <mnjcqwi@outlook.com>
2022-09-21 19:40:53 +0800

    test :

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@ local packer = require("packer")
 packer.startup({
   function(use)
    -- Packer 可以管理自己本身
-    use("wbthomason/packer.nvim")
+      use("wbthomason/packer.nvim")
    --你的插件列表...
     use("folke/tokyonight.nvim")
     use({
